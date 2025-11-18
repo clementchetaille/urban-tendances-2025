9fa57c54c845a7962db68c5d0e64a43de634e431
Clement Chetaille <clement.chetaille@gmail.com>
2025-11-18 20:02:42 +0100

    fix: rebuild all files in production mode

--- js/search-data.js
+++ js/search-data.js
@@ -3,366 +3,366 @@ const productsData = [
     "name": "Assises Thal S",
     "collection": "Thal S",
     "type": "assises",
-    "url": "/dist/produits/assises/thal-s-1.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/thal-s-1.html"
   },
   {
     "name": "Assises Thal S",
     "collection": "Thal S",
     "type": "assises",
-    "url": "/dist/produits/assises/thal-s-2.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/thal-s-2.html"
   },
   {
     "name": "Assises Thal S",
     "collection": "Thal S",
     "type": "assises",
-    "url": "/dist/produits/assises/thal-s-3.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/thal-s-3.html"
   },
   {
     "name": "Assises Thal S",
     "collection": "Thal S",
     "type": "assises",
-    "url": "/dist/produits/assises/thal-s-4.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/thal-s-4.html"
   },
   {
     "name": "Corbeilles Thal S",
     "collection": "Thal S",
     "type": "corbeilles",
-    "url": "/dist/produits/corbeilles/thal-s.html"
+    "url": "/urban-tendances-2025/dist/produits/corbeilles/thal-s.html"
   },
   {
     "name": "Jardinieres Thal S",
     "collection": "Thal S",
     "type": "jardinieres",
-    "url": "/dist/produits/jardinieres/thal-s.html"
+    "url": "/urban-tendances-2025/dist/produits/jardinieres/thal-s.html"
   },
   {
     "name": "Potelets Thal S",
     "collection": "Thal S",
     "type": "potelets",
-    "url": "/dist/produits/potelets/thal-s.html"
+    "url": "/urban-tendances-2025/dist/produits/potelets/thal-s.html"
   },
   {
     "name": "Barrieres Thal S",
     "collection": "Thal S",
     "type": "barrieres",
-    "url": "/dist/produits/barrieres/thal-s.html"
+    "url": "/urban-tendances-2025/dist/produits/barrieres/thal-s.html"
   },
   {
     "name": "Assises Bulle",
     "collection": "Bulle",
     "type": "assises",
-    "url": "/dist/produits/assises/bulle.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/bulle.html"
   },
   {
     "name": "Potelets Bulle",
     "collection": "Bulle",
     "type": "potelets",
-    "url": "/dist/produits/potelets/bulle.html"
+    "url": "/urban-tendances-2025/dist/produits/potelets/bulle.html"
   },
   {
     "name": "Barrieres Bulle",
     "collection": "Bulle",
     "type": "barrieres",
-    "url": "/dist/produits/barrieres/bulle.html"
+    "url": "/urban-tendances-2025/dist/produits/barrieres/bulle.html"
   },
   {
     "name": "Supportsvelos Bulle",
     "collection": "Bulle",
     "type": "supportsvelos",
-    "url": "/dist/produits/supportsvelos/bulle.html"
+    "url": "/urban-tendances-2025/dist/produits/supportsvelos/bulle.html"
   },
   {
     "name": "Assises Enjoy",
     "collection": "Enjoy",
     "type": "assises",
-    "url": "/dist/produits/assises/enjoy.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/enjoy.html"
   },
   {
     "name": "Potelets Enjoy",
     "collection": "Enjoy",
     "type": "potelets",
-    "url": "/dist/produits/potelets/enjoy.html"
+    "url": "/urban-tendances-2025/dist/produits/potelets/enjoy.html"
   },
   {
     "name": "Corbeilles Enjoy",
     "collection": "Enjoy",
     "type": "corbeilles",
-    "url": "/dist/produits/corbeilles/enjoy.html"
+    "url": "/urban-tendances-2025/dist/produits/corbeilles/enjoy.html"
   },
   {
     "name": "Jardinieres Enjoy",
     "collection": "Enjoy",
     "type": "jardinieres",
-    "url": "/dist/produits/jardinieres/enjoy-1.html"
+    "url": "/urban-tendances-2025/dist/produits/jardinieres/enjoy-1.html"
   },
   {
     "name": "Jardinieres Enjoy",
     "collection": "Enjoy",
     "type": "jardinieres",
-    "url": "/dist/produits/jardinieres/enjoy-2.html"
+    "url": "/urban-tendances-2025/dist/produits/jardinieres/enjoy-2.html"
   },
   {
     "name": "Cendriers Enjoy",
     "collection": "Enjoy",
     "type": "cendriers",
-    "url": "/dist/produits/cendriers/enjoy.html"
+    "url": "/urban-tendances-2025/dist/produits/cendriers/enjoy.html"
   },
   {
     "name": "Supportsvelos Enjoy",
     "collection": "Enjoy",
     "type": "supportsvelos",
-    "url": "/dist/produits/supportsvelos/enjoy.html"
+    "url": "/urban-tendances-2025/dist/produits/supportsvelos/enjoy.html"
   },
   {
     "name": "Transatsdivans Escale",
     "collection": "Escale",
     "type": "transatsdivans",
-    "url": "/dist/produits/transatsdivans/escale-1.html"
+    "url": "/urban-tendances-2025/dist/produits/transatsdivans/escale-1.html"
   },
   {
     "name": "Transatsdivans Escale",
     "collection": "Escale",
     "type": "transatsdivans",
-    "url": "/dist/produits/transatsdivans/escale-2.html"
+    "url": "/urban-tendances-2025/dist/produits/transatsdivans/escale-2.html"
   },
   {
     "name": "Paravents Escale",
     "collection": "Escale",
     "type": "paravents",
-    "url": "/dist/produits/paravents/escale.html"
+    "url": "/urban-tendances-2025/dist/produits/paravents/escale.html"
   },
   {
     "name": "Assises Jeux & Co",
     "collection": "Jeux & Co",
     "type": "assises",
-    "url": "/dist/produits/assises/jeux&co.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/jeux&co.html"
   },
   {
     "name": "Assises Littérature",
     "collection": "Littérature",
     "type": "assises",
-    "url": "/dist/produits/assises/litterature&co.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/litterature&co.html"
   },
   {
     "name": "Assises Mosaïque",
     "collection": "Mosaïque",
     "type": "assises",
-    "url": "/dist/produits/assises/mosaique-1.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/mosaique-1.html"
   },
   {
     "name": "Assises Mosaïque",
     "collection": "Mosaïque",
     "type": "assises",
-    "url": "/dist/produits/assises/mosaique-2.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/mosaique-2.html"
   },
   {
     "name": "Assises Mosaïque",
     "collection": "Mosaïque",
     "type": "assises",
-    "url": "/dist/produits/assises/mosaique-3.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/mosaique-3.html"
   },
   {
     "name": "Assises Mosaïque",
     "collection": "Mosaïque",
     "type": "assises",
-    "url": "/dist/produits/assises/mosaique-4.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/mosaique-4.html"
   },
   {
     "name": "Corbeilles Mosaïque",
     "collection": "Mosaïque",
     "type": "corbeilles",
-    "url": "/dist/produits/corbeilles/mosaique.html"
+    "url": "/urban-tendances-2025/dist/produits/corbeilles/mosaique.html"
   },
   {
     "name": "Jardinieres Mosaïque",
     "collection": "Mosaïque",
     "type": "jardinieres",
-    "url": "/dist/produits/jardinieres/mosaique.html"
+    "url": "/urban-tendances-2025/dist/produits/jardinieres/mosaique.html"
   },
   {
     "name": "Potelets Mosaïque",
     "collection": "Mosaïque",
     "type": "potelets",
-    "url": "/dist/produits/potelets/mosaique.html"
+    "url": "/urban-tendances-2025/dist/produits/potelets/mosaique.html"
   },
   {
     "name": "Barrieres Mosaïque",
     "collection": "Mosaïque",
     "type": "barrieres",
-    "url": "/dist/produits/barrieres/mosaique.html"
+    "url": "/urban-tendances-2025/dist/produits/barrieres/mosaique.html"
   },
   {
     "name": "Supportsvelos Mosaïque",
     "collection": "Mosaïque",
     "type": "supportsvelos",
-    "url": "/dist/produits/supportsvelos/mosaique.html"
+    "url": "/urban-tendances-2025/dist/produits/supportsvelos/mosaique.html"
   },
   {
     "name": "Tables Origine",
     "collection": "Origine",
     "type": "tables",
-    "url": "/dist/produits/tables/origine.html"
+    "url": "/urban-tendances-2025/dist/produits/tables/origine.html"
   },
   {
     "name": "Bornes Origine",
     "collection": "Origine",
     "type": "bornes",
-    "url": "/dist/produits/bornes/origine.html"
+    "url": "/urban-tendances-2025/dist/produits/bornes/origine.html"
   },
   {
     "name": "Potelets Origine",
     "collection": "Origine",
     "type": "potelets",
-    "url": "/dist/produits/potelets/origine.html"
+    "url": "/urban-tendances-2025/dist/produits/potelets/origine.html"
   },
   {
     "name": "Assises Printemps",
     "collection": "Printemps",
     "type": "assises",
-    "url": "/dist/produits/assises/printemps-1.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/printemps-1.html"
   },
   {
     "name": "Assises Printemps",
     "collection": "Printemps",
     "type": "assises",
-    "url": "/dist/produits/assises/printemps-2.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/printemps-2.html"
   },
   {
     "name": "Assises Printemps",
     "collection": "Printemps",
     "type": "assises",
-    "url": "/dist/produits/assises/printemps-3.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/printemps-3.html"
   },
   {
     "name": "Assises Printemps",
     "collection": "Printemps",
     "type": "assises",
-    "url": "/dist/produits/assises/printemps-4.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/printemps-4.html"
   },
   {
     "name": "Corbeilles Printemps",
     "collection": "Printemps",
     "type": "corbeilles",
-    "url": "/dist/produits/corbeilles/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/corbeilles/printemps.html"
   },
   {
     "name": "Jardinieres Printemps",
     "collection": "Printemps",
     "type": "jardinieres",
-    "url": "/dist/produits/jardinieres/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/jardinieres/printemps.html"
   },
   {
     "name": "Potelets Printemps",
     "collection": "Printemps",
     "type": "potelets",
-    "url": "/dist/produits/potelets/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/potelets/printemps.html"
   },
   {
     "name": "Barrieres Printemps",
     "collection": "Printemps",
     "type": "barrieres",
-    "url": "/dist/produits/barrieres/printemps-1.html"
+    "url": "/urban-tendances-2025/dist/produits/barrieres/printemps-1.html"
   },
   {
     "name": "Barrieres Printemps",
     "collection": "Printemps",
     "type": "barrieres",
-    "url": "/dist/produits/barrieres/printemps-2.html"
+    "url": "/urban-tendances-2025/dist/produits/barrieres/printemps-2.html"
   },
   {
     "name": "Transatsdivans Printemps",
     "collection": "Printemps",
     "type": "transatsdivans",
-    "url": "/dist/produits/transatsdivans/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/transatsdivans/printemps.html"
   },
   {
     "name": "Grillesdarbre Printemps",
     "collection": "Printemps",
     "type": "grillesdarbre",
-    "url": "/dist/produits/grillesdarbre/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/grillesdarbre/printemps.html"
   },
   {
     "name": "Lampes Printemps",
     "collection": "Printemps",
     "type": "lampes",
-    "url": "/dist/produits/lampes/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/lampes/printemps.html"
   },
   {
     "name": "Cendriers Printemps",
     "collection": "Printemps",
     "type": "cendriers",
-    "url": "/dist/produits/cendriers/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/cendriers/printemps.html"
   },
   {
     "name": "Bornes Printemps",
     "collection": "Printemps",
     "type": "bornes",
-    "url": "/dist/produits/bornes/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/bornes/printemps.html"
   },
   {
     "name": "Supportsvelos Printemps",
     "collection": "Printemps",
     "type": "supportsvelos",
-    "url": "/dist/produits/supportsvelos/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/supportsvelos/printemps.html"
   },
   {
     "name": "Tables Printemps",
     "collection": "Printemps",
     "type": "tables",
-    "url": "/dist/produits/tables/printemps.html"
+    "url": "/urban-tendances-2025/dist/produits/tables/printemps.html"
   },
   {
     "name": "Recuperateursdepiles Respire",
     "collection": "Respire",
     "type": "recuperateursdepiles",
-    "url": "/dist/produits/recuperateursdepiles/respire.html"
+    "url": "/urban-tendances-2025/dist/produits/recuperateursdepiles/respire.html"
   },
   {
     "name": "Tables Silva",
     "collection": "Silva",
     "type": "tables",
-    "url": "/dist/produits/tables/silva-1.html"
+    "url": "/urban-tendances-2025/dist/produits/tables/silva-1.html"
   },
   {
     "name": "Tables Silva",
     "collection": "Silva",
     "type": "tables",
-    "url": "/dist/produits/tables/silva-2.html"
+    "url": "/urban-tendances-2025/dist/produits/tables/silva-2.html"
   },
   {
     "name": "Assises Silva",
     "collection": "Silva",
     "type": "assises",
-    "url": "/dist/produits/assises/silva-1.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/silva-1.html"
   },
   {
     "name": "Assises Silva",
     "collection": "Silva",
     "type": "assises",
-    "url": "/dist/produits/assises/silva-2.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/silva-2.html"
   },
   {
     "name": "Assises Silva",
     "collection": "Silva",
     "type": "assises",
-    "url": "/dist/produits/assises/silva-3.html"
+    "url": "/urban-tendances-2025/dist/produits/assises/silva-3.html"
   },
   {
     "name": "Transatsdivans Silva",
     "collection": "Silva",
     "type": "transatsdivans",
-    "url": "/dist/produits/transatsdivans/silva.html"
+    "url": "/urban-tendances-2025/dist/produits/transatsdivans/silva.html"
   },
   {
     "name": "Cendriers Tulipe",
     "collection": "Tulipe",
     "type": "cendriers",
-    "url": "/dist/produits/cendriers/tulipe.html"
+    "url": "/urban-tendances-2025/dist/produits/cendriers/tulipe.html"
   },
   {
     "name": "Jardinieres Tulipe",
     "collection": "Tulipe",
     "type": "jardinieres",
-    "url": "/dist/produits/jardinieres/tulipe.html"
+    "url": "/urban-tendances-2025/dist/produits/jardinieres/tulipe.html"
   }
 ];
